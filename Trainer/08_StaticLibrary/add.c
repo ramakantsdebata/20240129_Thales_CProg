1ccdddf9a74d9bfbee3deb242103aed3d911a777
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-01-31 11:47:20 +0530

    Makefiles and Static Library

---
+++ Trainer/08_StaticLibrary/add.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <mmath.h>
+
+int add(int a, int b)
+{
+    printf("Adding...\n");
+    return a + b;
+}
---
+++ Trainer/09_SLibConsumer/build.sh
@@ -0,0 +1,1 @@
+gcc client.c -g -o client.out -I ../08_StaticLibrary/include/ -L ../08_StaticLibrary/ -lmmath
