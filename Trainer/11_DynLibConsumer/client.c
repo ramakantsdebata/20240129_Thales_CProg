2e0b2b0b12ada9cf0912cf8aec8db70f1ce0a3ac
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-01-31 14:05:23 +0530

    Dynamic Library

---
+++ Trainer/11_DynLibConsumer/client.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <mmath.h>
+
+int main()
+{
+	int result_add = add(5, 3);
+	int result_subtract = subtract(8, 2);
+
+	printf("Addition result: %d\n", result_add);
+	printf("Subtraction result: %d\n", result_subtract);
+
+	return 0;
+}
